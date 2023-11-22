85bcdf2f487d3e9117b5241c0389889e9feb8583
gloriaadanna <gloriaagwam7@gmail.com>
2023-11-22 10:55:07 -0800

    update

--- models/place.py
+++ models/place.py
@@ -9,6 +9,11 @@ from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
+association_table = Table("place_amenity", Base.metadata, Column("place_id",
+    string(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+    Column("amenity_id", string(60), ForeignKey("amenities.id"),
+        primary_key=True, nullable=False))
+
 
 class Place(BaseModel):
     """ A place to stay """
