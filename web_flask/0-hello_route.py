691ae4c2ecfc4392e6accdf0ab1f7fbdeceb4924
katiamiyu <katiamiyu@gmail.com>
2024-02-18 17:19:24 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-
-
-"""
-This is a python a script that starts a Flask web application running at port 5000
+"""lunch a flask web application run at port 5000
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """returns string when root route is
-       queried
+@app.route('/', strict_slashes=False)
+def hello_flask():
+    """display a static string when queried
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
